251fd591e6d2808c443c54a0f79fab629610814b
Karol Fabjańczuk <karol.fabjanczuk@gmail.com>
2018-11-05 14:53:42 +0100

    fix: release script working also on e.g. ubuntu

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 # Get new tags from remote
 git fetch --tags
 # Get latest tag name
-latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+latestTag=$(git describe --abbrev=0 --tags)
 # remove 'v' from latestTag
-latestTag=${latestTag//v}
+latestTag=${latestTag#"v"}
 
 echo "Enter release version (latest is $latestTag): "
 read VERSION
@@ -16,13 +16,13 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing $VERSION ..."
+    echo "Releasing $VERSION ..."
 
-  # commit
+    # commit
   npm version $VERSION --message "[release] $VERSION"
 
-  # publish
-  git push origin refs/tags/v$VERSION
-  git push
-  npm publish
+    # publish
+    git push origin refs/tags/v$VERSION
+    git push
+    npm publish
 fi
